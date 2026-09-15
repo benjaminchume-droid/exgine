6a844183dea9e0d3947b712b32bfb65fea30af79
benjaminchume-droid <benjaminchume@gmail.com>
2026-09-15 06:45:55 +0100

    feat: integrate streamed world into runtime

--- include/exgine/runtime.hpp
+++ include/exgine/runtime.hpp
@@ -3,6 +3,7 @@
 #include "exgine/ir.hpp"
 #include "exgine/material.hpp"
 #include "exgine/resources.hpp"
+#include "exgine/world.hpp"
 #include <cstdint>
 #include <memory>
 #include <string>
@@ -15,5 +16,5 @@ struct Transform{float x=0,y=0,z=0;};
 struct Entity{EntityId id=invalid_entity;NodeKind kind=NodeKind::Property;std::string name;Transform transform{};bool active=true;std::vector<Property> properties;std::shared_ptr<MeshAssembly> geometry;};
 class EntityRegistry{public:EntityId create(NodeKind,std::string);bool destroy(EntityId) noexcept;[[nodiscard]] Entity* get(EntityId) noexcept;[[nodiscard]] const Entity* get(EntityId) const noexcept;[[nodiscard]] std::size_t size() const noexcept{return entities_.size();}void clear() noexcept;private:EntityId next_id_=1;std::unordered_map<EntityId,Entity> entities_;};
 struct WorldState{EntityId world_entity=invalid_entity;EntityRegistry entities;std::uint64_t tick=0;double elapsed_seconds=0;};
-class Runtime{public:Runtime()=default;bool load(const IR&);void update(double) noexcept;void reset() noexcept;bool attach_geometry(EntityId,MeshAssembly);bool define_material(Material,TextureGenerationSettings settings={});[[nodiscard]] const Material* material(std::string_view) const noexcept;[[nodiscard]] std::shared_ptr<const MaterialResource> material_resource(std::string_view) const noexcept;[[nodiscard]] const MaterialLibrary& materials() const noexcept{return materials_;}[[nodiscard]] const ResourceCache& resources() const noexcept{return resources_;}[[nodiscard]] const WorldState& state() const noexcept{return state_;}[[nodiscard]] WorldState& state() noexcept{return state_;}private:WorldState state_;MaterialLibrary materials_;ResourceCache resources_;bool loaded_=false;};
+class Runtime{public:Runtime()=default;bool load(const IR&);void update(double) noexcept;void reset() noexcept;bool attach_geometry(EntityId,MeshAssembly);bool define_material(Material,TextureGenerationSettings settings={});bool stream_world(Vec3 focus_position,std::uint32_t view_radius_chunks);[[nodiscard]] const ProceduralWorld* world() const noexcept{return world_.get();}[[nodiscard]] const WorldStreamer* world_streamer() const noexcept{return streamer_.get();}[[nodiscard]] const Material* material(std::string_view) const noexcept;[[nodiscard]] std::shared_ptr<const MaterialResource> material_resource(std::string_view) const noexcept;[[nodiscard]] const MaterialLibrary& materials() const noexcept{return materials_;}[[nodiscard]] const ResourceCache& resources() const noexcept{return resources_;}[[nodiscard]] const WorldState& state() const noexcept{return state_;}[[nodiscard]] WorldState& state() noexcept{return state_;}private:WorldState state_;MaterialLibrary materials_;ResourceCache resources_;std::unique_ptr<ProceduralWorld> world_;std::unique_ptr<WorldStreamer> streamer_;bool loaded_=false;};
 } // namespace exgine
