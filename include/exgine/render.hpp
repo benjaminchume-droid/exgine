a6ebf20725ef53496cf7407c28c7e8ee4ebeb8a3
benjaminchume-droid <benjaminchume@gmail.com>
2026-09-15 17:56:06 +0100

    fix: restore mutable render texture bindings

--- include/exgine/render.hpp
+++ include/exgine/render.hpp
@@ -23,7 +23,7 @@ bool finite_matrix(const Mat4&)noexcept;
 struct Bounds3{Vec3 min{0,0,0},max{0,0,0};[[nodiscard]]bool valid()const noexcept;};
 Bounds3 mesh_bounds(const Mesh&)noexcept;
 Bounds3 transform_bounds(const Bounds3&,const Mat4&)noexcept;
-struct RenderMaterialBinding{Material material{};std::shared_ptr<const TextureSet> textures;[[nodiscard]]bool valid()const noexcept;};
+struct RenderMaterialBinding{Material material{};std::shared_ptr<TextureSet> textures;[[nodiscard]]bool valid()const noexcept;};
 struct RenderDrawCall{std::uint64_t entity_id=0;SceneNodeId scene_node=invalid_scene_node;RenderPass pass=RenderPass::Opaque;std::shared_ptr<const MeshAssembly> geometry;std::size_t part_index=0;std::shared_ptr<const SkinnedMesh> skinned_mesh;std::vector<Mat4> bone_palette;Mat4 model=Mat4::identity();Bounds3 world_bounds{};RenderMaterialBinding material{};[[nodiscard]]bool animated()const noexcept{return static_cast<bool>(skinned_mesh);}[[nodiscard]]bool valid()const noexcept;};
 struct RenderFrame{std::uint64_t frame_id=0;RenderConfig config{};Camera camera{};Mat4 view=Mat4::identity(),projection=Mat4::identity(),view_projection=Mat4::identity();RenderLightingSettings lighting{};std::vector<Light> lights;std::vector<RenderDrawCall> draws;[[nodiscard]]bool valid()const noexcept;};
 struct RenderStats{std::size_t submitted_draws=0,visible_draws=0,culled_draws=0,submitted_lights=0,animated_draws=0;};
