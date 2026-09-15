2dacbe6f64c86df1756aa3459ce35a0974827179
benjaminchume-droid <benjaminchume@gmail.com>
2026-09-15 07:10:11 +0100

    feat: add shader program contracts

---
+++ include/exgine/shaders.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+namespace exgine {
+enum class ShaderStage : std::uint8_t { Vertex, Fragment };
+struct ShaderProgram {
+    std::string name;
+    std::string vertex_source;
+    std::string fragment_source;
+    [[nodiscard]] bool valid() const noexcept;
+};
+class ShaderLibrary {
+public:
+    ShaderLibrary();
+    [[nodiscard]] const ShaderProgram* find(std::string_view name) const noexcept;
+    [[nodiscard]] std::size_t size() const noexcept { return programs_.size(); }
+private:
+    std::unordered_map<std::string, ShaderProgram> programs_;
+};
+[[nodiscard]] ShaderProgram make_pbr_shader();
+[[nodiscard]] ShaderProgram make_unlit_shader();
+} // namespace exgine
